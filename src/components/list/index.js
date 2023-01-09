ae38301c3c7ee24372a0e3d119e3ae7afcd07cc2
luis <luistenan@gmail.com>
2023-01-09 11:29:39 -0400

    Add feature to render List of photos
    Add ItemList component logic.
    Add List component logic

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,9 +1,37 @@
 import React from "react";
 
-function ListItem() {
-  return <div>list</div>;
+function ListItem({ id, alt, photographer, srcImg, urlWebOrigin }) {
+  return (
+    <figure className="item">
+      <a
+        className="item__link"
+        href={urlWebOrigin}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className="item__img" src={srcImg} alt={alt} />
+        <figcaption className="item__photographer">{photographer}</figcaption>
+      </a>
+    </figure>
+  );
 }
 
-export default function List() {
-  return <div>list</div>;
+export default function List({ images }) {
+  return (
+    <div>
+      hola
+      {images.map((image) => {
+        return (
+          <ListItem
+            id={image.id}
+            alt={image.alt}
+            photographer={image.photographer}
+            srcImg={image.src.medium}
+            urlWebOrigin={image.url}
+            key={image.id}
+          ></ListItem>
+        );
+      })}
+    </div>
+  );
 }
