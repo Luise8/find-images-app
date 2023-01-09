62114caa78fd9375fe2766857abad52f638d214f
luis <luistenan@gmail.com>
2023-01-09 09:38:35 -0400

    Add feature to get curated data from API
    Add  hook to get curated data from API
    Add conditional rendering to process loading and no found results

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import Header from "components/header";
+import useCuratedImages from "hooks/use-curated-images";
 
 export default function Home() {
-  return <div>home</div>;
+  const { images, loading, pageResult } = useCuratedImages();
+  return (
+    <div>
+      home
+      {loading == true ? (
+        "Loading" /* component spinner loading */
+      ) : images.length === 0 || images === [] || images === undefined ? (
+        "There are no results. Reload the page."
+      ) : (
+        <img src={images[1].src.small} alt="" /> /* List */
+      )}
+    </div>
+  );
 }
