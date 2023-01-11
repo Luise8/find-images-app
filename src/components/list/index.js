7dc3c62842f5ac2e79c2fb3d7364f787f3655a91
luis <luistenan@gmail.com>
2023-01-11 18:30:59 -0400

    Add masonry feature
    Add Mui;
    Add masonry feature to List component

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Masonry from "@mui/lab/Masonry";
 
 function ListItem({ id, alt, photographer, srcImg, urlWebOrigin }) {
   return (
@@ -9,7 +10,13 @@ function ListItem({ id, alt, photographer, srcImg, urlWebOrigin }) {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img className="item__img" src={srcImg} alt={alt} />
+        <img
+          className="item__img"
+          src={`${srcImg}?w=250&auto=format`}
+          srcSet={`${srcImg}?w=250&auto=format&dpr=2 2x`}
+          alt={alt}
+          loading="lazy"
+        />
         <figcaption className="item__photographer">{photographer}</figcaption>
       </a>
     </figure>
@@ -18,19 +25,38 @@ function ListItem({ id, alt, photographer, srcImg, urlWebOrigin }) {
 
 export default function List({ images }) {
   return (
-    <div className="lists">
-      {images.map((image) => {
-        return (
-          <ListItem
-            id={image.id}
-            alt={image.alt}
-            photographer={image.photographer}
-            srcImg={image.src.medium}
-            urlWebOrigin={image.url}
-            key={image.id}
-          ></ListItem>
-        );
-      })}
+    <div
+      className="lists"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        className="conatiner-list"
+        style={{
+          padding: "30px",
+          width: "900px",
+          minHeight: "829px",
+          backgroundColor: "#eee",
+        }}
+      >
+        <Masonry columns={3} spacing={2}>
+          {images.map((image) => {
+            return (
+              <ListItem
+                //sx={{ width: "200px" }}
+                id={image.id}
+                alt={image.alt}
+                photographer={image.photographer}
+                srcImg={image.src.original}
+                urlWebOrigin={image.url}
+                key={image.id}
+              ></ListItem>
+            );
+          })}
+        </Masonry>
+      </div>
     </div>
   );
 }
