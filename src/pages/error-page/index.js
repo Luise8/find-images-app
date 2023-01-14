e505ce53666ddf0f1cf78c648bfd78ff40908e9a
luis <luistenan@gmail.com>
2023-01-14 15:58:23 -0400

    Ad styles: positioning, sizing, font

--- src/pages/error-page/index.js
+++ src/pages/error-page/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Button from "components/button";
 import { Link } from "react-router-dom";
+import "pages/error-page/styles.scss";
 
 export default function ErrorPage() {
   return (
-    <div>
-      <p>404 Error</p>
+    <div className="error">
+      <div className="error__container-title">
+        <h2 className="error__title">404 Error</h2>
+      </div>
+
       <Link to="/">
         <Button>Go back home</Button>
       </Link>
