735fa78af2bf0afd436007c71642985399bd86f2
luis <luistenan@gmail.com>
2023-02-24 13:27:56 -0400

    Fix service to get curated images
    Remove filter by category and by query so that the images are more varied.
    Change to 15 results per page.

--- src/services/get-curated-images.js
+++ src/services/get-curated-images.js
@@ -1,7 +1,7 @@
 export default async function getCuratedImages(page = 1) {
   try {
     const res = await fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&image_type=photo&category=people&page=${page}&q=yellow+flowe&per_page=3`
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&image_type=photo&page=${page}&per_page=15`
     );
     const data = await res.json();
     return data;
